c65e8c7b46e7204fb7173d47d8452576acc0a3b9
DESKTOP-MTK7BFR\Daniel <momico24@hotmail.com>
2023-03-03 08:53:58 +0100

    CRUD comunidades funcionando

---
+++ BackendU2W/BackendU2W/Controllers/ComunidadesController.cs
@@ -0,0 +1,80 @@
+﻿using BackendU2W.DbContext;
+using BackendU2W.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendU2W.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComunidadesController : Controller
+    {
+        public readonly ApplicationDbContext _contexto;
+
+        public ComunidadesController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: api/<ComunidadesController>
+        [HttpGet]
+        public async Task<IEnumerable<Comunidades>> Get()
+            => await _contexto.Comunidades.Where(comunidad => comunidad.delete_date == null).ToListAsync();
+
+        // GET api/<ComunidadesController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Comunidades), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var comunidad = await _contexto.Comunidades.FindAsync(id);
+            return comunidad == null ? NotFound() : Ok(comunidad);
+        }
+
+        // POST api/<ComunidadesController>
+        [HttpPost]
+        public async Task<IActionResult> Post(Comunidades comunidad)
+        {
+            comunidad.create_date = DateTime.Now;
+            await _contexto.Comunidades.AddAsync(comunidad);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = comunidad.id_com }, comunidad);
+        }
+
+        // PUT api/<ComunidadesController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Put(int id, Comunidades comunidad)
+        {
+            if (id != comunidad.id_com) return BadRequest();
+
+            comunidad.last_modified = DateTime.Now;
+
+            _contexto.Entry(comunidad).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<ComunidadesController>/5
+        [HttpPut("{id}/delete")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comunidadToDelete = await _contexto.Comunidades.FindAsync(id);
+
+            if (comunidadToDelete == null) return NotFound();
+
+            comunidadToDelete.delete_date = DateTime.Now;
+
+            _contexto.Entry(comunidadToDelete).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- BackendU2W/BackendU2W/Models/Comunidades.cs
+++ BackendU2W/BackendU2W/Models/Comunidades.cs
@@ -1,6 +1,42 @@
-﻿namespace BackendU2W.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendU2W.Models
 {
+    [Table("Tbl_Comunidades")]
     public class Comunidades
     {
+        [Key]
+        public int id_com { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string nombre { get; set; }
+
+        [DefaultValue("Descripcion de la comunidad")]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Este campo debe tener un mínimo de 1 caracter y máximo de 1000")]
+        public string? descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Este campo debe tener un mínimo de 8 caractéres y máximo de 50")]
+        public string clave { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string picture { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string banner { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime create_date { get; set; }
+
+        [DataType(DataType.Date)]
+        [DefaultValue(null)]
+        public Nullable<DateTime> last_modified { get; set; }
+
+        [DataType(DataType.Date)]
+        [DefaultValue(null)]
+        public Nullable<DateTime> delete_date { get; set; }
     }
 }
