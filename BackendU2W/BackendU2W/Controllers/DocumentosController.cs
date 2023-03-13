08747943b6165aff5c47e76dc21786cd7cbba59e
DESKTOP-MTK7BFR\Daniel <momico24@hotmail.com>
2023-03-13 02:22:49 +0100

    Añadidos los dos controladores que faltaban

---
+++ BackendU2W/BackendU2W/Controllers/DocumentosController.cs
@@ -0,0 +1,80 @@
+﻿using BackendU2W.DbContext;
+using BackendU2W.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendU2W.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentosController : Controller
+    {
+        public readonly ApplicationDbContext _contexto;
+
+        public DocumentosController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: api/<DocumentosController>
+        [HttpGet]
+        public async Task<IEnumerable<Documentos>> Get()
+            => await _contexto.Documentos.Where(doc => doc.delete_date == null).ToListAsync();
+
+        // GET api/<DocumentosController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Documentos), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var doc = await _contexto.Documentos.FindAsync(id);
+            return doc == null || doc.delete_date != null ? NotFound() : Ok(doc);
+        }
+
+        // POST api/<DocumentosController>
+        [HttpPost]
+        public async Task<IActionResult> Post(Documentos doc)
+        {
+            doc.create_date = DateTime.Now;
+            await _contexto.Documentos.AddAsync(doc);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = doc.id_doc }, doc);
+        }
+
+        // PUT api/<DocumentosController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Put(long id, Documentos doc)
+        {
+            if (id != doc.id_doc) return BadRequest();
+
+            doc.last_modified = DateTime.Now;
+
+            _contexto.Entry(doc).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<DocumentosController>/5
+        [HttpPut("{id}/delete")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var docToDelete = await _contexto.Documentos.FindAsync(id);
+
+            if (docToDelete == null) return NotFound();
+
+            docToDelete.delete_date = DateTime.Now;
+
+            _contexto.Entry(docToDelete).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BackendU2W/BackendU2W/Controllers/PerfilesController.cs
@@ -0,0 +1,89 @@
+﻿using BackendU2W.DbContext;
+using BackendU2W.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendU2W.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilesController : Controller
+    {
+        public readonly ApplicationDbContext _contexto;
+
+        public PerfilesController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: api/<PerfilesController>
+        [HttpGet]
+        public async Task<IEnumerable<Perfiles>> Get()
+            => await _contexto.Perfiles.Where(perfil => perfil.delete_date == null).ToListAsync();
+
+        // GET api/<PerfilesController>/usuarioId/5
+        //[HttpGet("/usuarioId")]
+        //[ProducesResponseType(typeof(Perfiles), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> GetUserById(Perfiles perfil)
+        //{
+        //    var usuario = await _contexto.Usuarios.FindAsync(perfil.usuarioPropietario.id_usu);
+        //    return usuario == null || usuario.delete_date != null ? NotFound() : Ok(usuario);
+        //}
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Perfiles), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var perfil = await _contexto.Perfiles.FindAsync(id);
+            return perfil == null || perfil.delete_date != null ? NotFound() : Ok(perfil);
+        }
+
+        // POST api/<PerfilesController>
+        [HttpPost]
+        public async Task<IActionResult> Post(Perfiles perfil)
+        {
+            perfil.create_date = DateTime.Now;
+            await _contexto.Perfiles.AddAsync(perfil);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = perfil.id_perfil }, perfil);
+        }
+
+        // PUT api/<PerfilesController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Put(int id, Perfiles perfil)
+        {
+            if (id != perfil.id_perfil) return BadRequest();
+
+            perfil.last_modified = DateTime.Now;
+
+            _contexto.Entry(perfil).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<PerfilesController>/5
+        [HttpPut("{id}/delete")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var perfilToDelete = await _contexto.Perfiles.FindAsync(id);
+
+            if (perfilToDelete == null) return NotFound();
+
+            perfilToDelete.delete_date = DateTime.Now;
+
+            _contexto.Entry(perfilToDelete).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
